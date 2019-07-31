cc4319c82db0a825c52926c91e7910f7a8746b72
Annie <anniekao@posteo.net>
2019-07-31 16:21:13 -0400

    Initial commit / Added completed page fetcher app

---
+++ fetcher.js
@@ -0,0 +1,44 @@
+const args = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const writeToFile = (filePath, body) => {
+  fs.writeFile(filePath, body, err => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+};
+
+const promptOverwrite = (filePath, body) => {
+  rl.question('Overwrite existing file? (Y/N)', answer => {
+    if (answer === 'Y') {
+      writeToFile(filePath, body);
+    } else {
+      console.log('File has not been overwritten.');
+    }
+    rl.close();
+  });
+};
+
+const fetcher = (args) => {
+  const src = args[0];
+  const filePath = args[1];
+  request(src, (error, response, body) => {
+    fs.exists(filePath, (exists) => {
+      if (exists) {
+        promptOverwrite(filePath, body);
+      } else {
+        writeToFile(filePath, body);
+      }
+    });
+  });
+
+};
+
+fetcher(args);
